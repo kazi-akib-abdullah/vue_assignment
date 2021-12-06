4d4a4b737b4e4e7a8d2117d86d0139e30c944fc8
Kazi Akib Abdullah <45953236+kazi-akib-abdullah@users.noreply.github.com>
2021-12-06 14:09:51 +0600

    update flight body

--- src/components/FlightBodyDiv.vue
+++ src/components/FlightBodyDiv.vue
@@ -6,12 +6,8 @@
     <div class="row">
       <div class="col-8 justify-content-start ps-5">
         <div>
-          <a
-            class="nav-link active text-secondary"
-            style="text-decoration: none"
-            href=""
-            ><h4><span class="border-bottom border-3">Flights</span></h4></a
-          >
+          <a class="nav-link active text-secondary" style="text-decoration: none" href="">
+            <h4><span class="border-bottom border-3">Flights</span></h4></a>
           <h5 class="text-primary">
             <ul style="overflow: hidden">
               <li class="pe-5" style="display: inline-block">Reedem flights</li>
@@ -20,79 +16,38 @@
           </h5>
         </div>
         <div>
-          <form action="">
+          <form method="POST" @submit.prevent="functionName">
             <div class="card-group">
               <div class="card btn">
                 <div class="card-body text-secondary">
                   <h6 class="card-title text-secondary">Leaving from</h6>
-                  <div
-                    class="form-control me-2 button btn-outline"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal1"
-                    data-toggle="myModal"
-                  >
-                    <h3>DAC</h3>
-                    <h5 class="text-secondary">Dhaka</h5>
+                  <div class="form-control me-2 button btn-outline" data-bs-toggle="modal" data-bs-target="#exampleModal1" data-toggle="myModal">
+                    <h3>{{ leavingFrom }}</h3>
+                    <h5 class="text-secondary">{{ leavingFrom }}</h5>
                   </div>
-
-                  <!-- <Modal :modalActive= "modalActive">
-                    <div class="modal-content">
-                      <h1>Modal Header</h1>
-                      <p>Paragraph</p>
-                    </div>
-                  </Modal>
-                  <button @click="toggleModal" type="button">Open Modal</button> -->
                 </div>
               </div>
               <div class="card btn">
                 <div class="card-body">
                   <h6 class="card-title text-secondary">Going to</h6>
-                  <div
-                    class="form-control me-2 button btn-outline"
-                    data-bs-toggle="modal"
-                    data-bs-target="#exampleModal1"
-                    data-toggle="myModal"
-                  >
-                    <h3>DAC</h3>
-                    <h5 class="text-secondary">Dhaka</h5>
+                  <div class="form-control me-2 button btn-outline" data-bs-toggle="modal" data-bs-target="#exampleModal1" data-toggle="myModal">
+                    <h3>{{ goingTo }}</h3>
+                    <h5 class="text-secondary">{{ goingTo }}</h5>
                   </div>
                 </div>
               </div>
               <div class="card btn">
                 <div id="FlightBodyDiv" class="text-secondary">
                   <h6 id="FlightBodyDiv" class="card-title">Departing on</h6>
-                  <date-picker
-                    v-model="range"
-                    lang="en"
-                    :clearable="true"
-                    range
-                    type="date"
-                    width="500"
-                  ></date-picker>
-
+                  <input type="date" v-model="range1" >
                   <h6 class="card-title">Returning on</h6>
-                  <date-picker
-                    v-model="range"
-                    lang="en"
-                    :clearable="true"
-                    range
-                    type="date"
-                    width="500"
-                  ></date-picker>
+                  <input type="date" v-model="range2" name="" id="">
                 </div>
               </div>
               <div class="card btn" style=" background: linear-gradient(315deg,#005d63 0,#202f42 99.93%);">
                 <div class="card-body">
-                  <i
-                    style="position: absolute; top: 60px; right: 90px"
-                    class="fas fa-fighter-jet fa-2x text-white"
-                  ></i>
-                  <h5
-                    style="position: absolute; bottom: 15px"
-                    class="card-title text-white fixed-bottom"
-                  >
-                    Search flights
-                  </h5>
+                  <i style="position: absolute; top: 50px; right: 80px" class="fas fa-fighter-jet fa-2x text-white"></i>
+                  <h5><input style="position: absolute; bottom: 0px; left:10px; background: linear-gradient(315deg,#005d63 0,#202f42 99.93%);" class="card-title text-white fixed-bottom" type="submit" value="Search Flights"></h5>
                 </div>
               </div>
             </div>
@@ -101,119 +56,42 @@
         <div class="pt-3">
           <h5 class="text-primary">
             <ul style="overflow: hidden">
-              <li class="pe-5" style="display: inline-block">
-                Return <i class="fas fa-angle-down"></i>
-              </li>
-              <li style="display: inline-block">
-                Economy, 1 Adult <i class="fas fa-angle-down"></i>
-              </li>
+              <li class="pe-5" style="display: inline-block"> Return <i class="fas fa-angle-down"></i></li>
+              <li style="display: inline-block"> Economy, 1 Adult <i class="fas fa-angle-down"></i></li>
             </ul>
           </h5>
         </div>
         <div>
           <div class="card-group">
-            <div
-              class="card btn"
-              style="
-                background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);
-              "
-            >
+            <div class="card btn" style=" background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);">
               <div class="card-body d-flex justify-content-center pb-4">
-                <i
-                  class="fas fa-list-ul text-white border"
-                  style="
-                    display: inline-block;
-                    border-radius: 50px;
-                    padding: 0.8em 0.8em;
-                  "
-                ></i>
-                <h6
-                  style="position: absolute; bottom: 0px"
-                  class="card-title text-white pt-3"
-                >
-                  Manage booking
-                </h6>
+                <i class="fas fa-list-ul text-white border" style=" display: inline-block; border-radius: 50px; padding: 0.8em 0.8em;"></i>
+                <h6 style="position: absolute; bottom: 0px" class="card-title text-white pt-3"> Manage booking</h6>
               </div>
             </div>
-            <div
-              class="card btn"
-              style="
-                background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);
-              "
-            >
+            <div class="card btn" style=" background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);">
               <div class="card-body d-flex justify-content-center pb-4">
-                <i
-                  class="fas fa-sticky-note text-white border"
-                  style="
-                    display: inline-block;
-                    border-radius: 50px;
-                    padding: 0.8em 0.8em;
-                  "
-                ></i>
-                <h6
-                  style="position: absolute; bottom: 0px"
-                  class="card-title text-white"
-                >
-                  Check in
-                </h6>
+                <i class="fas fa-sticky-note text-white border" style=" display: inline-block; border-radius: 50px; padding: 0.8em 0.8em;"></i>
+                <h6 style="position: absolute; bottom: 0px" class="card-title text-white"> Check in</h6>
               </div>
             </div>
-            <div
-              class="card btn"
-              style="
-                background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);
-              "
-            >
+            <div class="card btn" style=" background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);">
               <div class="card-body d-flex justify-content-center pb-4">
-                <i
-                  class="fas fa-exchange-alt text-white border"
-                  style="
-                    display: inline-block;
-                    border-radius: 50px;
-                    padding: 0.8em 0.8em;
-                  "
-                ></i>
-                <h6
-                  style="position: absolute; bottom: 0px"
-                  class="card-title text-white"
-                >
-                  Rebook and refund
-                </h6>
+                <i class="fas fa-exchange-alt text-white border" style=" display: inline-block; border-radius: 50px; padding: 0.8em 0.8em;"></i>
+                <h6 style="position: absolute; bottom: 0px" class="card-title text-white"> Rebook and refund</h6>
               </div>
             </div>
-            <div
-              class="card btn"
-              style="
-                background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);
-              "
-            >
+            <div class="card btn" style=" background: linear-gradient(315deg, #005d63 0, #202f42 99.93%);">
               <div class="card-body d-flex justify-content-center pb-4">
-                <i
-                  class="fa fa-place-of-worship text-white border"
-                  style="
-                    display: inline-block;
-                    border-radius: 50px;
-                    padding: 0.8em 0.8em;
-                  "
-                ></i>
-                <h6
-                  style="position: absolute; bottom: 0px"
-                  class="card-title text-white"
-                >
-                  COVID-19 updates
-                </h6>
+                <i class="fa fa-place-of-worship text-white border" style="display: inline-block; border-radius: 50px; padding: 0.8em 0.8em;"></i>
+                <h6 style="position: absolute; bottom: 0px" class="card-title text-white"> COVID-19 updates</h6>
               </div>
             </div>
           </div>
         </div>
       </div>
       <div class="col-4 justify-content-end">
-        <img
-          src="../assets/beach.jpg"
-          alt="Beach"
-          style="height: 600px; weight: 400px"
-          srcset=""
-        />
+        <img src="../assets/beach.jpg" alt="Beach" style="height: 600px; weight: 400px" srcset=""/>
       </div>
     </div>
   </div>
@@ -230,37 +108,21 @@
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title justify-content-center text-primary" id="exampleModalLabel">Select City</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form method="POST" @submit.prevent="functionName">
+        <form>
         <div class="modal-body">   
             <div class="mb-3">
-              <label for="recipient-name1" class="col-form-label text-start"
-                >Going to:</label
-              >
+              <label for="recipient-name1" class="col-form-label text-start">Going to:</label>
               <input type="text" class="form-control" id="recipient-name1" v-on:keyup="this.depIata" v-model="goingTo" />
             </div>
             <div class="mb-3">
-              <label for="recipient-name" class="col-form-label"
-                >Leaving From:</label
-              >
+              <label for="recipient-name" class="col-form-label">Leaving From:</label>
               <input type="text" class="form-control" id="recipient-name" v-on:onkeyup="this.desIdata" v-model="leavingFrom"/>
-            </div>
-
-          
+            </div>          
         </div>
         <div class="modal-footer">
-          <input
-            type="submit"
-            class="btn btn-secondary" 
-            data-bs-dismiss="modal"  value="Apply"
-          />
-          <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+          <input type="" class="btn btn-secondary" data-bs-dismiss="modal"  value="Apply"/>
         </div>
         </form>
       </div>
@@ -268,18 +130,16 @@
   </div>
 </template>
 <script>
-import DatePicker from "vue3-datepicker";
-// import Axios from "vue-axios";
-import axios from 'axios';
-// import HotelDatePicker from 'vue-hotel-datepicker';
-// import 'vue-hotel-datepicker/dist/vueHotelDatepicker.css';
 
-// import DatePicker from "vue-datepicker";
+import axios from 'axios';
+
+
+
 // import Modal from '/src/components/Modal.vue'
 
 export default {
   components: {
-    DatePicker,
+
   },
   data() {
     return {
@@ -291,20 +151,74 @@ export default {
         end: "23:30",
       },
       datetime: "",
-      range: "",
+      range1: "",
+      range2: "",
       leavingFrom: "",
       goingTo: "",
       apiData: null
     };
   },
   methods: {
-    functionName: function async () {
-      
+    functionName: async function () {      
       console.log(this.leavingFrom + this.goingTo);
-      var url = "https://api.sharetrip.net/api/v1/flight/search?tripType=Return&adult=1&child=0&infant=0&class=Economy&origin=" + this.goingTo + "&destination=" + this.leavingFrom + "&depart=" + "2021-12-12" + "&depart=" + "2021-12-15"
-      axios.get(url).then(response => (this.apiData = response));
-      console.log(this.response);
+      var url = "https://api.sharetrip.net/api/v1/flight/search?tripType=Return&adult=1&child=0&infant=0&class=Economy&origin=" + this.goingTo + "&destination=" + this.leavingFrom + "&depart=" + this.range1 + "&depart=" + this.range2;
+        var maindata;
+        var destinationIataCode;
+        var originIataCode;
+        var destinationTime;
+        var originTime;
+        var duration;
+        var price;
+        var airlineCode;
+        var flightNumber;
       
+      // eslint-disable-next-line no-unused-vars
+      let response = await axios.get(url).then(function(response){return response})
+      // var price = response.data.flights[0].price;
+      // console.log(price);
+      console.log(response);
+                response.then(
+                    (data) => (
+                        (maindata = data),
+                        // console.log(data.response.flights[0].flight[0].originName.code)
+                        // console.log(maindata),
+                        (destinationIataCode =
+                            maindata.response.flights[0].flight[0].destinationName.code),
+                        (originIataCode =
+                            maindata.response.flights[0].flight[0].originName.code),
+                        (destinationTime =
+                            maindata.response.flights[0].flight[0].departureDateTime.time),
+                        (originTime =
+                            maindata.response.flights[0].flight[0].arrivalDateTime.time),
+                        (duration = maindata.response.flights[0].flight[0].duration),
+                        (price = maindata.response.flights[0].price),
+                        (airlineCode =
+                            maindata.response.flights[0].segments[0].segment[1].airlines
+                            .code),
+                        (flightNumber =
+                            maindata.response.flights[0].segments[0].segment[1]
+                            .flightNumber),
+                        console.log(
+                            destinationIataCode,
+                            "\n",
+                            originIataCode,
+                            "\n",
+                            destinationTime,
+                            "\n",
+                            originTime,
+                            "\n",
+                            duration,
+                            "\n",
+                            price,
+                            "\n",
+                            airlineCode,
+                            "\n",
+                            flightNumber
+                        )
+                    )
+                );
+                // console.log(response)
+            
     },
 
   }
